c509852fc7726bd51c93eb1ca7243a2b9ad685cf
Franklinson <franklinhassey@gmail.com>
2024-07-14 18:05:38 +0000

    Created the various pages for the todo app

--- todo/urls.py
+++ todo/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path('', views.TodoList, name='todo_list'),
+    path('<int:pk>/', views.TodoDetail, name='todo_detail'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from .models import Todo
 
-# Create your views here.
+
+def TodoList(request):
+    todos = Todo.objects.all()
+    total = todos.count()
+    completed = Todo.objects.filter(completed=True).count()
+    
+    context = {
+        'todos': todos,
+        'total': total,
+        'completed': completed,
+    }
+    return render(request, 'todo_list.html', context)
+
+
+def TodoDetail(request, pk):
+    todo = Todo.objects.get(id=pk)
+    context = {
+        'todo': todo,
+    }
+    return render(request, 'todo_detail.html', context)
