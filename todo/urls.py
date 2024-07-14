9a22e58e57c0e475c146ea8bd69a497398b9d5bf
Franklinson <franklinhassey@gmail.com>
2024-07-14 20:14:44 +0000

    Added authentication system

--- todo/urls.py
+++ todo/urls.py
@@ -1,10 +1,30 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('', views.TodoList, name='todo_list'),
+    path('/todo', views.TodoList, name='todo_list'),
     path('<int:pk>/', views.TodoDetail, name='todo_detail'),
     path('create/', views.createTodo, name='todo_create'),
     path('update/<int:pk>/', views.editTodo, name='todo_update'),
     path('delete/<int:pk>/', views.deleteTodo, name='todo_delete'),
+    
+    path('', views.login, name='login'),
+    path('register/', views.register, name='register'),
+    path('logout/', views.logoutuser, name='logout'),
+    path('reset_password/',
+     auth_views.PasswordResetView.as_view(template_name="password_reset.html"),
+     name="reset_password"),
+
+    path('reset_password_sent/', 
+        auth_views.PasswordResetDoneView.as_view(template_name="password_reset_sent.html"), 
+        name="password_reset_done"),
+
+    path('reset/<uidb64>/<token>/',
+     auth_views.PasswordResetConfirmView.as_view(template_name="password_reset_form.html"), 
+     name="password_reset_confirm"),
+
+    path('reset_password_complete/', 
+        auth_views.PasswordResetCompleteView.as_view(template_name="password_reset_done.html"), 
+        name="password_reset_complete"),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Todo
 from .forms import *
+from django.contrib.auth import authenticate, login as auth_login, logout
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 
 def TodoList(request):
@@ -15,7 +19,7 @@ def TodoList(request):
     }
     return render(request, 'todo_list.html', context)
 
-
+@login_required(login_url="/")
 def TodoDetail(request, pk):
     todo = Todo.objects.get(id=pk)
     context = {
@@ -23,7 +27,7 @@ def TodoDetail(request, pk):
     }
     return render(request, 'todo_detail.html', context)
 
-
+@login_required(login_url="/")
 def createTodo(request):
     form = TodoForm()
     if request.method == 'POST':
@@ -36,7 +40,7 @@ def createTodo(request):
     }
     return render(request, 'todo_form.html', context)
 
-
+@login_required(login_url="/")
 def editTodo(request, pk):
     todo = Todo.objects.get(id=pk)
     form = EditTodo(instance=todo)
@@ -50,10 +54,54 @@ def editTodo(request, pk):
     }
     return render(request, 'edit_todo.html', context)
 
-
+@login_required(login_url="/")
 def deleteTodo(request, pk):
     todo = get_object_or_404(Todo, pk=pk)
     if request.method == 'POST':
         todo.delete()
         return redirect('todo_list')
     return render(request, 'delete.html', {'rem': todo})
+
+
+
+def register(request):
+    
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            
+            username = form.cleaned_data.get('username')
+            
+            messages.success(request, 'Account was created successfully for ' + username)
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    
+    context = {'form': form,
+               }
+    return render(request, 'register.html', context)
+
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            auth_login(request, user)
+            return redirect('todo_list')
+        
+        else:
+            messages.info(request, 'Username or Password is incorrect')
+
+    context = {}
+    return render(request, 'login.html', context)
+
+
+@login_required(login_url="/")
+def logoutuser(request):
+    logout(request)
+    return redirect('login')
